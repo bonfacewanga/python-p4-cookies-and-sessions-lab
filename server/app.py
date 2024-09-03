697abeef4ccc44fb517fdfdda8f715fd1a396e68
Tangawizi <bonniewanga34@gmail.com>
2024-09-03 09:19:33 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, make_response, jsonify, session, request
 from flask_migrate import Migrate
 
 from models import db, Article, User
@@ -22,13 +22,35 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    response = make_response(
+        jsonify([article.to_dict() for article in articles]),
+        200
+    )
+    return response
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+
+    article = Article.query.filter_by(id=id).first() 
+    session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
+    session['page_views'] += 1
+    
+    if session['page_views'] <= 3: 
+
+        response = make_response(
+        jsonify(article.to_dict()),200)
+
+        return response
+    else:
+        limit = {'message': 'Maximum pageview limit reached'}
+
+        response = make_response(
+            limit,
+        401)
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
